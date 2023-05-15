4ea4079689b9bb9554b144a4605e26b44ad3a5ac
Jorge Sainz <bodybluebloody@gmail.com>
2023-05-15 11:40:55 +0200

    Ampliada la base de datos de productos

--- src/productsDB.js
+++ src/productsDB.js
@@ -22,6 +22,30 @@ const itemsDB = [
     description: 'Libro para que puedas pensar y desarrollar, con sentido, la paleta de colores que más se adecúa a tu marca.',
     pictureUrl: 'https://a2colores.es/wp-content/uploads/2020/02/Cabecera-2020-imagen-de-marca-800x400-1.jpg',
     category: 'freebies'
+  },
+  {
+    id: 'notionbasic',
+    title: 'Tu primer portal de Notion',
+    price: 0,
+    description: 'Portal de Notion básico para que comiences el cambio hacia el orden.',
+    pictureUrl: 'https://a2colores.es/wp-content/uploads/2020/02/Cabecera-2020-imagen-de-marca-800x400-1.jpg',
+    category: 'freebies'
+  },
+  {
+    id: 'notionpro',
+    title: 'Portal de Notino avanzado',
+    price: 250,
+    description: 'Portal de Notion para aquellos negocios que quieren llevar su organización al siguiente nivel.',
+    pictureUrl: 'https://a2colores.es/wp-content/uploads/2020/02/Cabecera-2020-imagen-de-marca-800x400-1.jpg',
+    category: 'digital'
+  },
+  {
+    id: 'plantillasLinkedin',
+    title: 'Linkedin profesional',
+    price: 120,
+    description: 'Haz que tus posts de Linkedin destaquen desde el minuto 1.',
+    pictureUrl: 'https://a2colores.es/wp-content/uploads/2020/02/Cabecera-2020-imagen-de-marca-800x400-1.jpg',
+    category: 'plantillas'
   }
 ]
 
